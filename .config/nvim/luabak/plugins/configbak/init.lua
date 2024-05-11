14ffbdd519d0b9de4a137e4c489887e67e05b31b
Blitzert <santoso.austin@gmail.com>
2024-05-11 17:02:05 +0800

    remove unused luabak files

--- .config/nvim/luabak/plugins/configbak/init.lua
+++
@@ -1,5 +0,0 @@
-require 'plugins/config/whichkey'
-require 'plugins/config/statusline'
-require 'plugins/config/nvim-tree-config'
-require 'plugins/config/telescope-config'
-require 'plugins/config/treesitter-config'
