1f25de8408c9467a5231995b336c5b3140d32ff8
Blitzert <santoso.austin@gmail.com>
2024-05-11 16:04:42 +0800

    fixed stuff

--- .config/zsh/tools/fzf.sh
+++ .config/zsh/tools/fzf.sh
@@ -10,8 +10,6 @@ fi
 eval "$(fzf --zsh)"
 
 # -- Use fd instead of fzf --
-alias fd=fdfind
-
 export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
